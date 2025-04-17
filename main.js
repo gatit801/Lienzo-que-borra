ebdf0a727649c78080a07fcfa5b86f99bcd225a2
gatit801 <Jramrom12@gmail.com>
2025-04-17 09:21:23 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -4,10 +4,11 @@ ctx = Lienzo.getContext("2d");
 
 color = "blue";
 relleno = "#0c16ac";
+grosor = "5";
 
 ctx.beginPath();
 ctx.strokeStyle = color;
-ctx.lineWidth = 5;
+ctx.lineWidth = grosor;
 ctx.fillStyle = relleno;
 ctx.arc(700, 103, 40, 0, 2*Math.PI);
 ctx.stroke();
@@ -18,6 +19,7 @@ Lienzo.addEventListener("mousedown", my_mousedown);
 function my_mousedown(e){
     color = document.getElementById("color").value;
     relleno = document.getElementById("color_relleno").value;
+    grosor = document.getElementById("linea_gros").value;
     mouse_x = e.clientX - Lienzo.offsetLeft
     mouse_y = e.clientY - Lienzo.offsetTop
     circle(mouse_x,mouse_y)
@@ -26,7 +28,7 @@ function my_mousedown(e){
 function circle(mouse_x,mouse_y){
     ctx.beginPath();
     ctx.strokeStyle = color;
-    ctx.lineWidth = 6;
+    ctx.lineWidth = grosor;
     ctx.fillStyle = relleno;
     ctx.arc(mouse_x, mouse_y, 40, 0, 2*Math.PI);
     ctx.stroke();
